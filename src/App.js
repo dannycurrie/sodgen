bb8b04661a1db3cb8378b66b186a1d2866679373
dannycurrie <danny.currie42@gmail.com>
2019-02-09 11:06:46 +0000

    add animated background gradient

--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import Sound from './components/Sound';
 import SoundContainer from './components/SoundContainer';
 import WithTrigger from './components/WithTrigger';
 import { sounds } from './utils/sounds';
 
+const Background = styled.div`
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  position: absolute;
+  height: 100%;
+  background-size: 200% 200%;
+  background-image: linear-gradient(to right, #aa4b6b, #6b6b83, #3b8d99);
+  animation: Gradient 15s ease infinite;
+
+  @-webkit-keyframes Gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+  @-moz-keyframes Gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+  @keyframes Gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
+
 const App = () => {
   useEffect(() => {});
 
   return (
-    <div className="App">
+    <Background className="App">
       <Sound soundId={sounds.drone} options={{ loop: true }} play />
       <Sound soundId={sounds.synthline} options={{ loop: true }} play />
       <WithTrigger>
         <Sound soundId="" />
       </WithTrigger>
       <SoundContainer />
-    </div>
+    </Background>
   );
 };
 
