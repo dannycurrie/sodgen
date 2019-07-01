d234d6273e350931637f53f679d09ae3299e8c37
dannycurrie <danny.currie42@gmail.com>
2019-07-01 19:26:15 +0100

    add follow on spotify button

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -17,10 +17,29 @@ const StyledLinks = styled.div`
       color: ${colours.textColour}BB;
     }
   }
+  & > iframe {
+    padding: 5px 10px 5px 10px;
+    color: ${colours.textColour}99;
+    text-decoration: none;
+    &:hover {
+      color: ${colours.textColour}BB;
+    }
+  }
 `;
 
 const Links = () => (
   <Fragment>
+    <StyledLinks>
+      <iframe
+        title="Follow on Spotify"
+        src="https://open.spotify.com/follow/1/?uri=spotify:artist:6sFIWsNpZYqfjUpaCgueju&size=basic&theme=dark&show-count=0"
+        width="92"
+        height="25"
+        scrolling="no"
+        frameBorder="0"
+        allowTransparency="true"
+      />
+    </StyledLinks>
     <StyledLinks>
       <a href="https://shockofdaylight.bandcamp.com/" target="blank">
         <FontAwesomeIcon icon="music" />
